d498e5d278b3ed5827afcdcefa807c776696e528
Laura <l.hernandez0509@gmail.com>
2020-08-17 21:06:15 -0500

    segundo commit

--- index.js
+++ index.js
@@ -1,101 +1,167 @@
+// const express = require('express')
+// const bodyParser = require('body-parser')
+
+// const app = express()
+// const port = 3000
+
+// const pets = require('./src/models/old-pets')
+// const Pets = require('./src/models/pets')
+
+// app.listen(port)
+
+// // Set up server to receive data from requests
+// app.use(express.json())
+// app.use(bodyParser.urlencoded({
+//   extended: true
+// }));
+
+// // Pet REST routes
+
+// app.get('/pets', (req, res) => {
+//   Pets.find({})
+//   .then((result)=>{
+//     res.status(200).json({ data: result })
+//   })
+//   .catch(()=>{
+//     res.status(500).json({error:'Error getting pets'})
+//   })
+//  // const result = pets.getAll()
+//   //console.log(result)
+//   //res.status(200).json({ data: result })
+// })
+// //using mongoose database queries, find 
+// app.get('/pets:id', (req, res) => {
+//   Pets.findById(req.params.id)
+//   .then((result)=>{
+//     res.status(200).json({ data: result })
+//   })
+//   .catch(()=>{
+//     res.status(500).json({error:'Error getting pets'})
+//   })
+
+// })
+
+// //mongoose database queries to create 
+// app.post('/pets', (req, res) => {
+//   const result = pets.create(req.body)
+//   if (pets) {
+//     res.status(200).json({ data: result })
+//   } else {
+//     res.status(500).json({ error: 'Error saving pet' })
+//   }
+// })
+
+
+
+// app.get('/pets/:id', (req, res) => {
+//   const result = pets.get(req.params.id)
+//   if(result) {
+//     res.status(200).json({ data: result })
+//   } else {
+//     res.status(404).json({ error: 'Pet not found' })
+//   }
+// })
+
+// app.post('/pets', (req, res) => {
+//   const result = pets.create(req.body)
+//   if (pets) {
+//     res.status(200).json({ data: result })
+//   } else {
+//     res.status(500).json({ error: 'Error saving pet' })
+//   }
+// })
+
+// app.put('/pets/:id', (req, res) => {
+//   const result = pets.update(req.params.id, req.body)
+//   if(result) {
+//     res.status(200).json({ data: result })
+//   } else {
+//     res.status(404).json({ error: 'Error updating pet' })
+//   }
+// })
+
+// app.delete('/pets/:id', (req, res) => {
+//   const result = pets.destroy(req.params.id)
+//   if (result) {
+//     res.status(200).json({ data: result })
+//   } else {
+//     res.status(404).json({ error: 'Error deleting pet' })
+//   }
+// })
+
+// app.get('/hello', (req, res) => {
+//   res.status(200).send('Hello world')
+// })
+
+// // Catch all other routes
+// app.use((req, res) => {
+//   res.status(404).send('Route not found')
+// })
+
+// console.log('RESTful API server started on: ' + port)
+
 const express = require('express')
 const bodyParser = require('body-parser')
-
 const app = express()
 const port = 3000
-
-const pets = require('./src/models/old-pets')
-const Pets = require('./src/models/pets')
-
+const Pet = require('./src/models/pets')
 app.listen(port)
-
 // Set up server to receive data from requests
 app.use(express.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-
 // Pet REST routes
-
 app.get('/pets', (req, res) => {
-  Pets.find({})
-  .then((result)=>{
+  Pets.find({ })
+  .then((result) => {
     res.status(200).json({ data: result })
   })
-  .catch(()=>{
-    res.status(500).json({error:'Error getting pets'})
+  .catch(() => {
+    res.status(500).json({ error: 'Error getting pets' })
   })
- // const result = pets.getAll()
-  //console.log(result)
-  //res.status(200).json({ data: result })
 })
-//using mongoose database queries, find 
-app.get('/pets:id', (req, res) => {
-  Pets.findById(req.params.id)
-  .then((result)=>{
-    res.status(200).json({ data: result })
-  })
-  .catch(()=>{
-    res.status(500).json({error:'Error getting pets'})
-  })
-
-})
-
-//mongoose database queries to create 
-app.post('/pets', (req, res) => {
-  const result = pets.create(req.body)
-  if (pets) {
-    res.status(200).json({ data: result })
-  } else {
-    res.status(500).json({ error: 'Error saving pet' })
-  }
-})
-
-
-
 app.get('/pets/:id', (req, res) => {
-  const result = pets.get(req.params.id)
-  if(result) {
+  Pets.findById(req.params.id)
+  .then((result) => {
     res.status(200).json({ data: result })
-  } else {
+  })
+  .catch(() => {
     res.status(404).json({ error: 'Pet not found' })
-  }
+  })
 })
-
 app.post('/pets', (req, res) => {
-  const result = pets.create(req.body)
-  if (pets) {
+  new Pet(req.body).save()
+  .then((result) => {
     res.status(200).json({ data: result })
-  } else {
-    res.status(500).json({ error: 'Error saving pet' })
-  }
+  })
+  .catch((error) => {
+    res.status(500).json({ message: 'Error saving pet', error })
+  })
 })
-
 app.put('/pets/:id', (req, res) => {
-  const result = pets.update(req.params.id, req.body)
-  if(result) {
+  Pets.findByIdAndUpdate(req.params.id, req.body, { new: true })
+  .then((result) => {
     res.status(200).json({ data: result })
-  } else {
+  })
+  .catch((error) => {
     res.status(404).json({ error: 'Error updating pet' })
-  }
+  })
 })
-
 app.delete('/pets/:id', (req, res) => {
-  const result = pets.destroy(req.params.id)
-  if (result) {
+  Pets.findByIdAndDelete(req.params.id)
+  .then((result) => {
     res.status(200).json({ data: result })
-  } else {
+  })
+  .catch((error) => {
     res.status(404).json({ error: 'Error deleting pet' })
-  }
+  })
 })
-
 app.get('/hello', (req, res) => {
   res.status(200).send('Hello world')
 })
-
 // Catch all other routes
 app.use((req, res) => {
   res.status(404).send('Route not found')
 })
-
 console.log('RESTful API server started on: ' + port)
